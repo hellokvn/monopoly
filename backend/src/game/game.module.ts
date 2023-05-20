605379a3023469532d2301c07e47c5585f1ad69b
Kevin Vogel <hellokevinvogel@gmail.com>
2023-05-20 17:36:30 +0700

    chore(backend): add helper class

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -4,9 +4,31 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Game, GameSchema } from './game.schema';
 import { GameService } from './game.service';
 import { GameSchedule } from './game.schedule';
+import { ActionModule } from './action/action.module';
+import { AuctionModule } from './auction/auction.module';
+import { BuyModule } from './buy/buy.module';
+import { ChatModule } from './chat/chat.module';
+import { DeptsModule } from './depts/depts.module';
+import { DiceModule } from './dice/dice.module';
+import { FieldModule } from './field/field.module';
+import { RentModule } from './rent/rent.module';
+import { TradeModule } from './trade/trade.module';
+import { GameHelper } from './game.helper';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Game.name, schema: GameSchema }])],
-  providers: [GameGateway, GameService, GameSchedule],
+  imports: [
+    MongooseModule.forFeature([{ name: Game.name, schema: GameSchema }]),
+    ActionModule,
+    AuctionModule,
+    BuyModule,
+    ChatModule,
+    DeptsModule,
+    DiceModule,
+    FieldModule,
+    RentModule,
+    TradeModule,
+  ],
+  providers: [GameGateway, GameService, GameSchedule, GameHelper],
+  exports: [GameHelper],
 })
 export class GameModule {}
