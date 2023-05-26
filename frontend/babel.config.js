db55548a1e5e268888fea7b141ebccac61754cf0
Kevin Vogel <hellokevinvogel@gmail.com>
2023-05-27 05:45:15 +0700

    config(frontend): modify config

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+  presets: ['@vue/cli-plugin-babel/preset'],
+  plugins: [
+    ['@babel/proposal-decorators', { legacy: true }],
+    ['@babel/proposal-class-properties', { loose: true }],
+  ],
 };
