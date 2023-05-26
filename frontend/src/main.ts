7682cf2c9cbf5513804b06ffaa2bcc30c815cff1
Kevin Vogel <hellokevinvogel@gmail.com>
2023-05-27 05:47:44 +0700

    chore(frontend): add global properties

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,34 @@
+import '@/assets/scss/main.scss';
+import axios from 'axios';
+import { io } from 'socket.io-client';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App);
+const headers: Record<string, string> = { 'Content-Type': 'application/json' };
+const baseURL: string = process.env.API_URL as string;
+
+function bootstrap(): void {
+  app.config.globalProperties.$axios = axios.create({ baseURL, headers });
+  app.config.globalProperties.$socket = io('ws://127.0.0.1:3000/game');
+  app.config.globalProperties.$filters = {
+    currency(value: number, currency = 'Gold'): string {
+      return `${value} ${currency}`;
+    },
+  };
+
+  app.use(store(app));
+  app.use(router);
+
+  app.directive('scroll', {
+    updated<T extends HTMLElement>(element: T) {
+      element.scrollTop = element.scrollHeight;
+    },
+  });
+
+  app.mount('#app');
+}
+
+bootstrap();
--- frontend/src/shims-vue.d.ts
+++ frontend/src/shims-vue.d.ts
@@ -1,6 +1,30 @@
 /* eslint-disable */
+import type { DefineComponent } from 'vue';
+import type { State } from './store';
+
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+declare module '@vue/runtime-core' {
+  import type { AxiosInstance } from 'axios';
+  import type { Socket } from 'socket.io-client';
+  interface ComponentCustomProperties {
+    $axios: AxiosInstance;
+    $socket: Socket;
+    $store: Store<State>;
+    $filters: { currency: (value: number, currency?: string) => string };
+  }
+}
+
+declare module 'vue' {
+  import type { AxiosInstance } from 'axios';
+  import type { Socket } from 'socket.io-client';
+  interface ComponentCustomProperties {
+    $axios: AxiosInstance;
+    $socket: Socket;
+    $store: Store<State>;
+    $filters: { currency: (value: number, currency?: string) => string };
+  }
 }
